4103bcb7f22ebc0db35d365c94a636c80fc7f332
drmeerkat <drmeerkatli@gmail.com>
2023-07-28 11:49:52 -0400

    remove extra rewards

--- procgen/src/games/conchaser.cpp
+++ procgen/src/games/conchaser.cpp
@@ -128,15 +128,13 @@ class ConfoundedChaserGame : public BasicAbstractGame {
 
         if (obj->type == LARGE_ORB) {
             eat_time = cur_time;
-            step_data.reward += ORB_REWARD*10;
+            step_data.reward += ORB_REWARD;
             obj->will_erase = true;
         } else if (obj->type == ENEMY) {
             if (can_eat_enemies()) {
                 obj->will_erase = true;
-                step_data.reward += 1.0;
             } else {
                 step_data.done = true;
-                step_data.reward += -0.1;
             }
         }
     }
