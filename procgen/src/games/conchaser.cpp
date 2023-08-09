7d8f435acf7017833ff1da70c33b169666a6b375
drmeerkat <drmeerkatli@gmail.com>
2023-08-09 11:40:37 -0400

    revert back to causal chaser

--- procgen/src/games/conchaser.cpp
+++ procgen/src/games/conchaser.cpp
@@ -101,17 +101,17 @@ class ConfoundedChaserGame : public BasicAbstractGame {
 
     int image_for_type(int type) override {
         if (type == ENEMY) {
-            if (can_eat_enemies()) {
+            // if (can_eat_enemies()) {
                 // needs to figure our its weakness status by its speed/locations
-                // enemies' looking doesn't change at all, always seems to be "weak"
+                // enemies' shape doesn't change at all, always seems to be "weak"
                 return ENEMY_WEAK;
-            } else {
-                // simulates the enemies walking movements
-                int rem = (cur_time / 2) % 4;
-                if (rem == 3)
-                    rem = 1;
-                return ENEMY + rem;
-            }
+            // } else {
+            //     // simulates the enemies walking movements
+            //     int rem = (cur_time / 2) % 4;
+            //     if (rem == 3)
+            //         rem = 1;
+            //     return ENEMY + rem;
+            // }
         }
 
         return BasicAbstractGame::image_for_type(type);
