7bb43917ae51e7527dd373baadc2e4343a670b20
Meerkat <drmeerkat@gmail.com>
2024-05-22 17:16:59 -0400

    tune default context

--- procgen/src/vecgame.cpp
+++ procgen/src/vecgame.cpp
@@ -343,8 +343,8 @@ VecGame::VecGame(int _nenvs, VecOptions opts) {
     // This will be updated by python API later on 
     // and the game will reset w.r.t the new one
     context.push_back(1);
-    context.push_back(1);
-    context.push_back(2);
+    context.push_back(0);
+    context.push_back(3);
     context.push_back(-1);
     context.push_back(0);
 
